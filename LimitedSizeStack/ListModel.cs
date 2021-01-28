ea71e4d3987e3559ec7b643689274f93ea8b5d5a
theBird2Bones <mr.kolmogortsev@gmail.com>
2021-01-28 18:33:29 +0500

    rename some things

--- LimitedSizeStack/ListModel.cs
+++ LimitedSizeStack/ListModel.cs
@@ -14,38 +14,38 @@ namespace TodoApplication
     {
         public List<TItem> Items { get; }
         public int Limit;
-        private LimitedSizeStack<ChangedItem<TItem>> StackToUndo;
+        private LimitedSizeStack<ChangedItem<TItem>> HistoryOfChanges;
         
 
         public ListModel(int limit)
         {
             Items = new List<TItem>();
             Limit = limit;
-            StackToUndo = new LimitedSizeStack<ChangedItem<TItem>>(Limit);
+            HistoryOfChanges = new LimitedSizeStack<ChangedItem<TItem>>(Limit);
         }
 
         public void AddItem(TItem item)
         {
             Items.Add(item);
-            StackToUndo.Push(new ChangedItem<TItem>(){Index = Items.Count-1, Item = item,OperationWasMade = '+'});
+            HistoryOfChanges.Push(new ChangedItem<TItem>(){Index = Items.Count-1, Item = item,OperationWasMade = '+'});
         }
 
         public void RemoveItem(int index)
         {
-            StackToUndo.Push(new ChangedItem<TItem>(){Index = index, Item = Items[index],OperationWasMade = '-'});
+            HistoryOfChanges.Push(new ChangedItem<TItem>(){Index = index, Item = Items[index],OperationWasMade = '-'});
             Items.RemoveAt(index);
         }
 
         public bool CanUndo(){
-            return StackToUndo.Count != 0;
+            return HistoryOfChanges.Count != 0;
         }
 
         public void Undo(){
-            var lastOperation = StackToUndo.Pop();
-            if (lastOperation.OperationWasMade == '+')
-                Items.RemoveAt(lastOperation.Index);
-            if (lastOperation.OperationWasMade == '-')
-                Items.Insert(lastOperation.Index,lastOperation.Item);
+            var lastChange = HistoryOfChanges.Pop();
+            if (lastChange.OperationWasMade == '+')
+                Items.RemoveAt(lastChange.Index);
+            if (lastChange.OperationWasMade == '-')
+                Items.Insert(lastChange.Index,lastChange.Item);
         }
     }
 }
